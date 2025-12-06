c5c94645d5079e32a84abce98e66b1ceeaac1e30
Jethro C <jethro@bothyculture.org>
2025-12-06 20:44:36 +0000

    fix: update reset.sh to accept osm-pbf file as argument

--- reset.sh
+++ reset.sh
@@ -2,10 +2,14 @@
 
 set -euo pipefail
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <osm-pbf-file>"
+    exit 1
+fi
 
 docker compose down
 sudo rm -rf /osm-data/*
-sudo cp ~/Downloads/chad-latest.osm.pbf ./data.osm.pbf
+sudo cp "$1" ./data.osm.pbf
 docker compose build
 docker compose up import
 docker compose up light dark
