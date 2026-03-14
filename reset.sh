5a8f78e24752a17b0de04d27cb5bdd4b173249b2
Jethro C <ben@bencollins.co.uk>
2026-03-14 09:42:58 +0000

    chore: remove unnecessary file copy command from reset script

--- reset.sh
+++ reset.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 
 docker compose down
 sudo rm -rf /osm-data/*
-sudo cp ~/Downloads/chad-latest.osm.pbf ./data.osm.pbf
 docker compose build
 docker compose up import
 docker compose up light dark
